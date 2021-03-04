f468d2e064ac7e57b009377b2a843f4e5b4cdfb7
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-04 17:14:12 +0100

    Create Cupboards of Kithgard.py

---
+++ Kithgard Dungeon/Cupboards of Kithgard.py
@@ -0,0 +1,9 @@
+# Il pourrait y avoir quelque-chose d'utile autour de vous!
+
+# D'abord, déplacez-vous jusqu'au Placard (Cupboard).
+hero.moveUp()
+hero.moveRight(2)
+
+# Puis, attaquez le "Cupboard" à l'aide d'une boucle while-true.
+while True:
+    hero.attack("Cupboard")
