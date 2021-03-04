a41ea816b7266ea6b74925818ef37eee3a31dc60
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-04 17:41:04 +0100

    Create Kithgard Gates.py

---
+++ Kithgard Dungeon/Kithgard Gates.py
@@ -0,0 +1,6 @@
+# Construisez 3 clôtures pour garder les ogres à distance!
+hero.moveDown()
+hero.buildXY("fence", 36, 34)
+hero.buildXY("fence", 36, 30)
+hero.buildXY("fence", 36, 26)
+hero.moveRight(3)
