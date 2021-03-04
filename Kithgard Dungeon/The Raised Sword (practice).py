89d4f92980c8cebf19a74486275346702669e995
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-04 11:33:45 +0100

    Create The Raised Sword (practice).py

---
+++ Kithgard Dungeon/The Raised Sword (practice).py
@@ -0,0 +1,9 @@
+# Vaincs les ogres.
+# Souviens-toi qu'ils doivent prendre chacun 2 coups avant d'être vaincus.
+
+hero.attack("Rig")
+hero.attack("Rig")
+hero.attack("Gurt")
+hero.attack("Gurt")
+hero.attack("Ack")
+hero.attack("Ack")
