50fca33b5a2a59d28430c1b9c46009011193be24
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-04 16:43:53 +0100

    Create Kithgard Librarian.py

---
+++ Kithgard Dungeon/Kithgard Librarian.py
@@ -0,0 +1,8 @@
+# Vous avez besoin d'un mot de passe pour ouvrir la porte de la bibliothèque!
+# Le mot de passe est dans le guide d'aide!
+# Cliquez sur le bouton "Aide" au-dessus de la fenêtre de code pour ouvrir le guide d'aide du niveau.
+# La plupart des niveaux ont un guide d'aide avec des informations supplémentaires. Si vous êtes coincés dans un niveau, n'hésitez pas à cliquer sur le bouton d'aide !
+
+hero.moveRight()
+hero.say("Hush")  # ∆
+hero.moveRight()
