fb4e9fd7ef4b450a970e3f74f38dd07fa363f6e4
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-04 16:47:30 +0100

    Create Haunted Kithmaze.py

---
+++ Kithgard Dungeon/Haunted Kithmaze.py
@@ -0,0 +1,7 @@
+# Les boucles (loops) sont le meilleur moyen de faire des choses répétitives.
+
+while True:
+    # Ajoute des commandes ici pour les répéter.
+    hero.moveRight(2)
+    hero.moveUp(2)
+    
