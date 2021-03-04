ddcfc7b31b6ee4242cc8dee337cf49cd1b7f3099
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-04 16:45:53 +0100

    Create Fire Dancing.py

---
+++ Kithgard Dungeon/Fire Dancing.py
@@ -0,0 +1,9 @@
+# Le code s'exécute normalement dans l'ordre où il est écrit.
+# Les boucles permettent de répéter un bloc de code plusieurs fois.
+# Tu dois appuyer sur tab (ou 4 fois sur espace) pour indenter une ligne dans la boucle.
+
+while True:
+    hero.moveRight()
+    # Ajouter ici une commande moveLeft à la boucle
+    hero.moveLeft()
+    
