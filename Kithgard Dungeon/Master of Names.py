fd1d28aa596f3220ae5a929bd3c43b1647d9b9bb
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-04 17:16:07 +0100

    Create Master of Names.py

---
+++ Kithgard Dungeon/Master of Names.py
@@ -0,0 +1,21 @@
+# Ton héros ne connaît pas le nom de ces ennemis !
+# Les lunettes te donnent la capacité de trouver l'ennemi le plus proche (findNearestEnemy).
+
+# Assigne le résultat de hero.findNearestEnemy() à la variable enemy1:
+enemy1 = hero.findNearestEnemy()
+
+# enemy1 maintenant fait référence à l'ennemi le plus proche. Utilise cette variable pour attaquer !
+hero.attack(enemy1)
+hero.attack(enemy1)
+
+# Maintenant que l'ennemi est battu, appelle  hero.findNearestEnemy()  de nouveau aura pour résultat de trouver le nouvel ennemi le plus proche.
+enemy2 = hero.findNearestEnemy()
+hero.attack(enemy2)
+hero.attack(enemy2)
+
+# Assigne le résultat de hero.findNearestEnemy()  à la variable enemy3:
+enemy = hero.findNearestEnemy()
+
+# Maintenant attaque en utilisant la variable enemy3:
+hero.attack(enemy)
+hero.attack(enemy)
