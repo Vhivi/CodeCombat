abd4cf0a6ba4730ddd8b7a71bd70fd3834e88928
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-04 17:15:20 +0100

    Create Known Enemy.py

---
+++ Kithgard Dungeon/Known Enemy.py
@@ -0,0 +1,14 @@
+# Tu peux utiliser une variable au même titre que le nom d'un ennemi.
+
+enemy1 = "Kratt"
+enemy2 = "Gert"
+enemy3 = "Ursa"
+
+hero.attack(enemy1)
+hero.attack(enemy1)
+
+hero.attack(enemy2)
+hero.attack(enemy2)
+
+hero.attack(enemy3)
+hero.attack(enemy3)
