3ca0a49456b15484b1a097b8e7546428d79ba649
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-06 11:12:15 +0100

    Create Woodland Cleaver.py

---
+++ Backwoods Forest/Woodland Cleaver.py
@@ -0,0 +1,13 @@
+# Utilisez votre nouvelle capacité "cleave" (fendre) aussi souvent que possible.
+
+hero.moveXY(23, 23)
+while True:
+    enemy = hero.findNearestEnemy()
+    if hero.isReady("cleave"):
+        # Fendez l'ennemi!
+        hero.cleave(enemy)
+        pass
+    else:
+        # Sinon (si fendre n'est pas prêt), faites une attaques normale.
+        hero.attack(enemy)
+        pass
