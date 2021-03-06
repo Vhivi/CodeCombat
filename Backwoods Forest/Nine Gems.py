d61896e8493d29644f673315634c18d7a8dd8a39
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-06 11:00:08 +0100

    Create Nine Gems.py

---
+++ Backwoods Forest/Nine Gems.py
@@ -0,0 +1,6 @@
+# Collecte toutes les gemmes  en seulement 4 moveXY ou moins!
+# Les programmeurs ont besoin d'être créatifs!
+hero.moveXY(32, 48)
+hero.moveXY(70, 48)
+hero.moveXY(32, 12)
+hero.moveXY(32, 36)
