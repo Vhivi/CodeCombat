87a675e13c52661380594a1b514edf68fd3dea6e
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-06 11:14:23 +0100

    Create Range Finder.py

---
+++ Backwoods Forest/Range Finder.py
@@ -0,0 +1,20 @@
+# Les ogres parcourent la forêt!
+# Utilise la méthode distanceTo pour trouver où se trouvent les ennemis.
+# Communique la distance de chaque ennemi pour dire à l'artillerie où tirer!
+
+enemy1 = "Gort"
+distance1 = hero.distanceTo(enemy1)
+hero.say(distance1)
+
+enemy2 = "Smasher"
+distance2 = hero.distanceTo(enemy2)
+# Communique (Say) la variable distance2!
+hero.say(distance2)
+# Ne tire pas sur tes amis!
+friend3 = "Charles"
+
+# The last ogre.
+enemy4 = "Gorgnub"
+# Trouve et communique la distance des ennemis.
+distance3 = hero.distanceTo(enemy4)
+hero.say(distance3)
