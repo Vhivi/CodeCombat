64ed97b72b92fcffd9f42ab003e318834e67c617
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-06 11:25:40 +0100

    Create Oasis.py

---
+++ Sarven Desert/Oasis.py
@@ -0,0 +1,18 @@
+# Avancez pour atteindre l'oasis
+# mais reculez pour éviter les Yaks a proximité
+while True:
+    x = hero.pos.x
+    y = hero.pos.y
+    enemy = hero.findNearestEnemy()
+    if enemy and hero.distanceTo(enemy) < 10:
+        # Déplacez-vous à gauche en soustrayant 10 de vos coordonnées X
+        left = x - 10
+        # Use moveXY to move to the new x, y position.
+        hero.moveXY(left, y)
+        pass
+    else:
+        # Déplacez-vous à droite en additionnant 10 à vos coordonnées X
+        right = x + 10
+        # Use moveXY to move to the new x, y position.
+        hero.moveXY(right, y)
+        pass
