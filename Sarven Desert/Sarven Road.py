7af2beb09c33154a9889ba3ad6411724434ed62e
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-06 11:37:47 +0100

    Create Sarven Road.py

---
+++ Sarven Desert/Sarven Road.py
@@ -0,0 +1,17 @@
+# Atteignez l'oasis. Attention aux nouveaux ennemis : les ogres éclaireurs !
+# Allez vers le haut et la droite en ajoutant votre position en x et en y.
+
+while True:
+    # Attaque tout les ennemis que tu croise.
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        distance = hero.distanceTo(enemy)
+        if hero.isReady("cleave") and distance < 10:
+            hero.cleave(enemy)
+        else:
+            hero.attack(enemy)
+    # Ou si il n'y à aucun ennemis en vue, continue d'avancer vers en haut à droite.
+    else:
+        hero.moveXY(hero.pos.x + 1, hero.pos.y + 1)
+    pass
+    
