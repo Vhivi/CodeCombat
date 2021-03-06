5bec68184fdcb9350200d2169cef8d09df11b2d3
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-06 11:02:31 +0100

    Create  Woodland Cubbies.py

---
+++ Backwoods Forest/Woodland Cubbies.py
@@ -0,0 +1,28 @@
+# Explorez la forêt, mais restez sur le qui-vive!
+# Il peut y avoir des ogres dans les recoins de forêt!
+
+hero.moveXY(19, 33)
+enemy = hero.findNearestEnemy()
+# L'instruction `if` testera si la variable contient un ogre.
+if enemy:
+    hero.attack(enemy)
+    hero.attack(enemy)
+
+hero.moveXY(49, 51)
+enemy = hero.findNearestEnemy()
+if enemy:
+    # Attaquez l'ennemi ici:
+    hero.attack(enemy)
+    hero.attack(enemy)
+    # `pass` est un caractère générique : c'est-à-dire qu'il n'a aucune fonction mis à part de fermer les boucles `if`.
+    pass
+
+hero.moveXY(58, 14)
+enemy = hero.findNearestEnemy()
+# Utilisez une instruction `if` pour voir s'il y a un ennemi.
+if enemy:
+    # S'il y a un ennemi, attaquez-le!    
+    hero.attack(enemy)
+    hero.attack(enemy)
+    pass
+    
