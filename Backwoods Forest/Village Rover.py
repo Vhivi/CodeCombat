6242733f4a971a877be65bf6588c8051570142e4
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-06 11:18:21 +0100

    Create  Village Rover.py

---
+++ Backwoods Forest/Village Rover.py
@@ -0,0 +1,16 @@
+# Ceci définit une fonction nommée findAndAttackEnemy
+def findAndAttackEnemy():
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        hero.attack(enemy)
+
+# Ce code ne fait pas partie de la fonction.
+while True:
+    # Maintenante tu peux patrouiller le village en utilisant findAndAttackEnemy
+    hero.moveXY(35, 34)
+    findAndAttackEnemy()
+    
+    # Maintenant déplace-toi vers l'entrée de droite.
+    hero.moveXY(60, 31)
+    # Utilise findAndAttackEnemy
+    findAndAttackEnemy()
