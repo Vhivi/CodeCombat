51aedbfc3ddac8015425d93cfcd68bb5fba54f20
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-06 11:20:50 +0100

    Create Backwoods Brawl 1.py

---
+++ Backwoods Forest/Backwoods Brawl 1.py
@@ -0,0 +1,19 @@
+# Reste en vie pendant 1 minute. 
+# Si tu gagne, ca devient plus difficile (mais la récompense est plus grande)
+# Si tu perds, tu devra attendre une journée pour rejouer.
+# Rappele-toi, chaque présentation obtient une nouvelle table aléatoire.
+
+def checkAndAttack(target):
+    if target:
+        if hero.isReady("cleave") and (hero.distanceTo(target) < 10):
+            hero.cleave(target)
+        elif hero.distanceTo(target) > 10:
+            hero.shield()
+            hero.attack(target)
+        else:
+            hero.attack(target)
+
+while True:
+    enemy = hero.findNearestEnemy()
+    checkAndAttack(enemy)
+
