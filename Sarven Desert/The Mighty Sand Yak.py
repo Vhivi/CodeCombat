3c4b62ab26305ec049537d8d6413ef47a679e08c
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-06 11:24:55 +0100

    Create The Mighty Sand Yak.py

---
+++ Sarven Desert/The Mighty Sand Yak.py
@@ -0,0 +1,18 @@
+# Laissez les yaks approcher, puis déplacez vous 10m tout droit pour esquiver.
+# Esquivez 4 yaks pour compléter le niveau.
+
+while True:
+    # Obtenez votre position actuelle (x et y) en utilisant votre Pierre de réflexion.
+    x = hero.pos.x
+    y = hero.pos.y
+    
+    # Trouve le yak le plus proche.
+    yak = hero.findNearestEnemy()
+    
+    # Utilisez "if" pour vous déplacer uniquement lorsqu'un yak est à 10m.
+    if hero.distanceTo(yak) < 10:
+        # Pour avancer tout droit, ajoutez 10 à votre position x.
+        right = x + 10
+        # Utilisez moveXY pour bouger !
+        hero.moveXY(right, y)
+        pass
