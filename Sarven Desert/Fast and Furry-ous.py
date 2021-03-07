e7b1a5d31408708c30990fd09ab5c1a8cbfac0f4
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-07 12:22:02 +0100

    Update Fast and Furry-ous.py
    Add docstring #4

--- Sarven Desert/Fast and Furry-ous.py
+++ Sarven Desert/Fast and Furry-ous.py
@@ -1,6 +1,7 @@
 # Use an event handler so pet and hero will both run!
 
 def petMove():
+    '''Définit la position que le pet doit atteindre.'''
     pet.moveXY(50, 21)
 
 # Use pet.on("spawn", petMove) instead of petMove().
