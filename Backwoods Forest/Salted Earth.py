2116b49054c508b5592cef518db7c6bf695d27cf
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-08 14:11:02 +0100

    Create Salted Earth.py

---
+++ Backwoods Forest/Salted Earth.py
@@ -0,0 +1,17 @@
+# Les ogres attaquent à proximité de la colonie !
+# Fais attention, car les ogres ont semé du poison dans la terre.
+# Ramasse des pièces et vaincs les ogres, mais fait attention aux pièges et au poison !
+
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy.type == "munchkin" or enemy.type == "thrower":
+        hero.attack(enemy)
+    item = hero.findNearestItem()
+    # Vérifie quel est le type de l'objet pour être sûre que le héro ne ramasse pas du poison !
+    # Si le type d'objet est une "gem" ou une "coin":
+    if item.type == "gem" or item.type == "coin":
+        # Ensuite déplace toi et ramasse le:
+        pos = item.pos
+        x = pos.x
+        y = pos.y
+        hero.moveXY(x, y)
