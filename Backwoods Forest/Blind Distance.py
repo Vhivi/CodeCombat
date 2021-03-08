70213e34022850ce01990ce8bf18544345e3afad
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-08 14:09:45 +0100

    Create Blind Distance.py

---
+++ Backwoods Forest/Blind Distance.py
@@ -0,0 +1,19 @@
+# Votre tâche est de lui dire la distance par rapport aux ogres qui approchent.
+
+# Cette fonction trouve l'ennemi le plus proche et retourne sa distance par rapport a vous. 
+# Si il n'y a pas d'ennemi, la fonction retourne 0.
+def nearestEnemyDistance():
+    enemy = hero.findNearestEnemy()
+    result = 0
+    if enemy:
+        result = hero.distanceTo(enemy)
+    return result
+
+while True:
+    # Appelle nearestEnemyDistance() et 
+    # Sauve le résultat dans la variable enemyDistance.
+    enemyDistance = nearestEnemyDistance()
+    # Si enemyDistance est supérieur à 0:
+    if enemyDistance > 0:
+        # Dis la valeur de la variable enemyDistance.
+        hero.say(enemyDistance)
