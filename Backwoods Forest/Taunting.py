8060341c1f77daf6d41f15a119791a880b0b04a9
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-08 14:31:45 +0100

    Update Taunting.py

--- Backwoods Forest/Taunting.py
+++ Backwoods Forest/Taunting.py
@@ -1,13 +1,13 @@
 # Attack les munchkins, appelle les brawlers et ignore les burls.
 
-def dealEnemy(enemy):
+def dealEnemy(character):
     '''Cette fonction définit le comportement des héros face aux ennemis.'''
     # Si enemy.type est "munchkin":
-    if enemy.type == "munchkin":
+    if character.type == "munchkin":
         # Ensuite attaque-le:
-        hero.attack(enemy)
+        hero.attack(character)
     # Si le type d'ennemi est "brawler":
-    elif enemy.type == "brawler":
+    elif character.type == "brawler":
         # Alors dis quelque chose au brawler.
         hero.say("Fuck You !")
 
