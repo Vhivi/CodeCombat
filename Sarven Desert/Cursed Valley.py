c88453111ba772003bceac90ea73430d28077777
Vhivi <38220028+Vhivi@users.noreply.github.com>
2021-03-08 14:53:58 +0100

    Create  Cursed Valley.py

---
+++ Sarven Desert/Cursed Valley.py
@@ -0,0 +1,14 @@
+# Il fait trop chaud dehors! A chaque seconde tu perds de la vie.
+
+while True:
+    enemy = hero.findNearestEnemy()
+    # Attack if enemy exists AND enemy.team is "ogres"
+    # AND enemy.type is "skeleton"
+    if enemy and enemy.team == "ogres" and enemy.type == "skeleton":
+        hero.attack(enemy)
+    
+    item = hero.findNearestItem()
+    # Collect if item exists AND item.type is "potion"
+    # AND hero.health is less than hero.maxHealth / 4
+    if item and (item.type == "potion") and (hero.health < (hero.maxHealth / 4)):
+        hero.moveXY(item.pos.x, item.pos.y)
